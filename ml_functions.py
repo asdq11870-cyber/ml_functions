48a1bcc431b028bd2d1d9f0d63bd17442ad3365f
asdq11870-cyber <asdq11870@gmail.com>
2026-05-19 16:25:20 +0100

    Implement batch evaluation for model
    Added batch evaluation function for model performance on train and test datasets.

--- ml_functions.py
+++ ml_functions.py
@@ -93,6 +93,46 @@ def eval_model(model: torch.nn.Module,
             "model_loss": loss.item(),
             "model_acc": acc}
 
+def batch_eval_model(model: torch.nn.Module,
+               train_data_loader: torch.utils.data.DataLoader,
+               test_data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               accuracy_fn,
+               device: torch.device):
+
+    def evaluate(data_loader):
+        loss, acc = 0, 0
+
+        model.eval()
+        with torch.inference_mode():
+            for x, y in tqdm(data_loader):
+                x, y = x.to(device), y.to(device)
+
+                y_logits = model(x)
+                y_pred_labels = y_logits.argmax(dim=1)
+
+                loss += loss_fn(y_logits, y).item()
+                acc += accuracy_fn(
+                    y_true=y,
+                    y_pred=y_pred_labels
+                )
+
+            loss /= len(data_loader)
+            acc /= len(data_loader)
+
+        return loss, acc
+
+    train_loss, train_acc = evaluate(train_data_loader)
+    test_loss, test_acc = evaluate(test_data_loader)
+
+    return {
+        "model_name": model.__class__.__name__,
+        "train_loss": train_loss,
+        "train_acc": train_acc,
+        "test_loss": test_loss,
+        "test_acc": test_acc
+    }
+
 def plot_transformed_images(image_paths, transform, n=3, seed=42):
     """Plots a series of random images from image_paths.
 
